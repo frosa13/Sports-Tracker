b33fcc2811019d40b52c0f94171ba104c209e5bb
Francisco Rosa <franciscorosa1309@gmail.com>
2024-09-21 20:12:55 +0100

    First commit

---
+++ Sports Tracker/Sports Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sports Tracker
+//
+//  Created by Francisco Rosa on 21/09/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
